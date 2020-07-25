7072bbf06e72a6c83ed3e82ac14a13f8b235611a
Olga <nauka.dinosaurus@gmail.com>
2020-07-25 14:23:06 +0200

    user delete

---
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -0,0 +1,21 @@
+package pl.coderslab.users;
+
+import pl.coderslab.utils.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/delete")
+public class UserDelete extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt( req.getParameter("id"));
+        UserDao userDao = new UserDao();
+        userDao.delete(id);
+        resp.sendRedirect("/user/list");
+    }
+}
